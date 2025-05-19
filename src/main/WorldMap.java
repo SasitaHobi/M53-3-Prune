36c4669524581056de308565072d7deb877213d2
Tamslay <matteo.glassey@heig-vd.ch>
2025-05-19 22:00:31 +0200

    Update WorldMap.java

--- src/main/WorldMap.java
+++ src/main/WorldMap.java
@@ -3,11 +3,8 @@ package main;
 public class WorldMap {
 Location wMap [][]= new Location[10][10];  
 
-   // method add Location int int
+    // method add Location int int
     // get Location int int
     // set PlayerLocation
     // getPlayerLocation
-
-    
-    
 }
