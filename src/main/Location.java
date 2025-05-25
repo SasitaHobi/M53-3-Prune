576d7ff381ae8c352aff17d524dea6a96bb46f63
PlumsPie <sarah.furrer@hes-so.ch>
2025-05-25 15:00:54 +0200

    Merge

--- src/main/Location.java
+++ src/main/Location.java
@@ -4,13 +4,9 @@ public class Location {
     private String name;
     private String desrc;
     // method unlock
-<<<<<<< Updated upstream
-     
-=======
 
     public Location (String name, String  descr){
         this.name=name;
         this.desrc=descr;
     }
->>>>>>> Stashed changes
 }
