64a92f254b821d9f627eea838485a35bdaef0d2b
Sasita Hobi <sasita.hobi@heig-vd.ch>
2025-06-09 18:45:42 +0200

    Update WorldMap.java

--- src/main/WorldMap.java
+++ src/main/WorldMap.java
@@ -15,18 +15,22 @@ public class WorldMap {
         Item treasure= new Item("Trésor", "Félicitations! Vous avez trouvé le trésor! Vous pouvez quitter le jeu grâce à la commande quit.");
         Item riddle1= new LetterItem("Mon premier", "Mon premier  est un surplus...");
 
+        Item riddle1 = new LetterItem("Mon premier", "...mon premier est un peu trop...");
+        Item epee = new Item("épée", "vous pouvez découper des trucs");
+        Item key = new KeyItem("key", "ceci est une clé");
+        Item treasure = new Item("treasure", "Félicitations! Vous avez trouvé le trésor!");
+        Item teleportCrystal = new Item("teleport crystal", "Un cristal magique permettant de se téléporter.");
 
-        map[0][0] = new Location("Prairie", "Ceci est une prairie.", false, potion);
         map[0][1] = new Location("Chemin", "Ceci est un chemin.", false, null);
         map[0][2] = new Location("Cascade", "Ceci est une cascade.", false, null);
         map[1][0] = new Location("Rivière", "Ceci est une rivière.", false, epee);
         map[1][1] = new Location("Lac", "Ceci est un lac.", false, null);
         map[1][2] = new Location("Forêt", "Ceci est une forêt.", false, null);
         map[2][0] = new Location("Clairière", "Ceci est une clairère.", false, null);
+        map[2][0] = new Location("Clairière", "Ceci est une clairière.", false, null);
         map[2][1] = new Location("Marais", "Ceci est un marais.", false, null);
         map[2][2] = new Location("Montagne", "Ceci est une montagne.", false, teleportCrystal);
         map[3][0] = new Location("Grotte", "Ceci est une grotte.", false, null);
-        map[3][1] = new Location("Sommet", "Ceci est un sommet.", false, riddle1);
         map[3][2] = new Location("Château", "Ceci est un château.", true, treasure);
     }
 
@@ -47,6 +51,7 @@ public class WorldMap {
     public void setPlayerLocation(int row, int col) {
         this.playerRow = row;
         this.playerCol = col;
+        map[row][col].setVisited(true);
     }
 
     public int getPlayerRow() {
@@ -61,7 +66,6 @@ public class WorldMap {
         return map;
     }
 
-    
     public Location[][] getRawMap() {
         return map;
     }
